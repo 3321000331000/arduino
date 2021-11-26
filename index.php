57fb04981a2dee872f46efaef96b44c1faecaecb
3321000331000 <prachya.teach@gmail.com>
2021-11-27 06:55:19 +0700

    first state

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+ <head>
+ <title>My College</title>
+ <link rel="stylesheet"  type="text/css"  href="css/style.css">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	
+ <meta charset = "utf-8">
+ <link rel="preconnect" href="https://fonts.gstatic.com">
+ <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
+        	
+
+
+	<link rel="stylesheet" href="css/grid.css">
+   <link rel="stylesheet" href="cssx/table.css">
+<style>
+
+</style>
+</head>
